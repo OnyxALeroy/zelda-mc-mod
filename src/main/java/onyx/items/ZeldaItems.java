c4f70a450ba343e2de1dd562d742e842a8168ecf
Antoine Dupret <antoinedupret6@gmail.com>
2025-04-17 01:05:46 +0200

    Adding an Item, with a name+texture+group

---
+++ src/main/java/onyx/items/ZeldaItems.java
@@ -0,0 +1,35 @@
+package onyx.items;
+
+import net.minecraft.util.Identifier;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+
+import java.util.function.Function;
+
+public class ZeldaItems {
+    // Registerer
+    	public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+		// Create the item key.
+		RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of("zelda-oot-mod", name));
+
+		// Create the item instance.
+		Item item = itemFactory.apply(settings.registryKey(itemKey));
+
+		// Register the item.
+		Registry.register(Registries.ITEM, itemKey, item);
+
+		return item;
+	}
+
+    public static final Item HOOKSHOT = register("hookshot", Item::new, new Item.Settings());
+
+    public static void initialize(){
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+		.register((itemGroup) -> itemGroup.add(ZeldaItems.HOOKSHOT));
+    }
+}
